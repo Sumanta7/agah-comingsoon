af9bb1ff8a2b22f66b799083a3dd1f2b3a17961b
Sumanta7 <ir.sumanta89@yahoo.com>
2022-04-07 15:38:08 +0430

    version 1.0

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css";
+    import './../../static/assets/bootstrap-grid.rtl.min (1).css'
+    import "./../../static/assets/global.css"
+    import "./../../static/assets/fonts/Yekan-Bakh.css"
+</script>
+  
+  <slot />
